47e44c89e26e2a2524bd034a06b49fe211e2bb7d
strangeqaergo <strangeqaergo@gmail.com>
2017-10-13 02:06:30 +0300

    intellij xdg-desktop-menu bug +

--- installs/keyboard_xorg.sh
+++ installs/keyboard_xorg.sh
@@ -1,6 +1,7 @@
 #@IgnoreInspection BashAddShebang
-#swtich language with capslock
-grep  -q -F microsoft ~/.xinitrc > /dev/null 2>&1  ||  echo 'setxkbmap -layout "us,ru" -option "grp:caps_toggle,grp_led:caps,numpad:microsoft"' > ~/.xinitrc
+# swtich language with capslock
+# investigate: fcitx https://github.com/trueos/lumina/issues/149
+grep  -q -F microsoft ~/.xinitrc > /dev/null 2>&1  ||  echo 'setxkbmap -layout "us,ru" -option "grp:caps_toggle,grp_led:caps,numpad:microsoft"' >> ~/.xinitrc
 
 
 
